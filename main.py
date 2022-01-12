b55e450b4fc714caf16ab7b2d0eea6f35d85074c
Ilya Yarusov <originalyia@gmail.com>
2022-01-12 14:35:58 +0800

    asdf

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ print('Hello, World!')
 
 # Просто добавил пояснение к коду, и еще кое-что.
 
-my_number = 1000
+my_number = 2000
 my_text = 'Мой текст!'
 
 def my_function():
-    new_text = my_text + ' И не чей другой!'
+    new_text = my_text + ' и ничей другой!'
     return new_text
 
 result = my_function()
